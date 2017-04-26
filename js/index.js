4b03189a75214af039bc474efadc46ca135f34bb
ZhouYaJie <609276681@qq.com>
2017-04-26 23:00:30 +0800

    magicStore

---
+++ js/index.js
@@ -0,0 +1,180 @@
+//loading
+var loadAndFetchSpine = {
+	init: function() {
+		this.spineItem;
+		this.spineInfoArr = [];
+		$('<div id="loading"></div>').prependTo($("section"));
+		for(var i = 0; i < 7; i++) {
+			$('<div class="loadItem"></div>').prependTo($("section").find('#loading'));
+		}
+
+		this.loadingAndBanner();
+		this.fetchSpineInfo();
+		this.showAllSpine();
+		this.downLoadSpine();
+	},
+	loadingAndBanner: function() {
+		var banner = $('.swiper-container.banner');
+		TweenMax.to(banner, 1, {
+			opacity: 1,
+			ease: Strong.easeOut,
+			onComplete: function() {
+				var bannerSwiper = new Swiper($('.swiper-container.banner'), {
+					autoplay: 3500,
+					loop: true,
+				});
+			}
+		});
+		var load = $('section').find(' #loading .loadItem');
+		TweenMax.staggerTo(load, .2, { scaleY: 1.6, repeat: -1, yoyo: true, ease: Linear.easeInOut }, 0.1);
+	},
+	loadedAnimate: function() {
+		this.LoadTimeOut = setTimeout(function() {
+			var loading = $('#loading');
+			TweenMax.to(loading, 1, {
+				opacity: 0,
+				ease: Strong.easeOut,
+				onComplete: function() {
+					loading.remove();
+				}
+			});
+
+		}, 400);
+	},
+	fetchSpineInfo: function() {
+		var that = this;
+		$.ajax({
+			type: "GET",
+			async: true,
+			url: "https://mapi.magic-store.cn/mstore/spine/tree/details",
+			success: function(res) {
+				console.log(res)
+				if(res.code == 200) {
+					that.spineItem = res.entities;
+					try {
+						window.MStore.spineDomains(res.pkgBucket, res.imgBucket, res.endpoint);
+					} catch(e) {
+						console.log(e.message)
+					}
+					that.item = res.entities;
+					that.imgBucket = res.imgBucket;
+					that.endpoint = res.endpoint;
+					that.createSpineItems(res.entities, res.imgBucket, res.endpoint);
+				}
+			}
+		});
+	},
+	spineLoadedList: function(list) {
+		console.log(list);
+		this.spineInfoArr = JSON.parse(list);
+	},
+	createSpineItems: function(item, imgBucket, endpoint) {
+		for(var i = 0; i < item.length; i++) {
+			var spineItemsWrap = $('<div class="classify swiper-container"></div>');
+			spineItemsWrap.attr('data-categoryid', item[i].id);
+			var spineItemsWrapCon = '<p><i>' + item[i].name + '</i><span class="showAll">显示全部</span></p> <div class="swiper-wrapper">'
+
+			var spineLength = 5;
+			if(item[i].spines.length <= spineLength) {
+				spineLength = item[i].spines.length;
+			}
+
+			for(var k = 0; k < spineLength; k++) {
+				spineItemsWrapCon += '<div class="swiper-slide" data-source="' + item[i].spines[k].source + '" data-id="' + item[i].spines[k].id + '">' +
+					'<img class="border" src="img/MS_icon_asset_02.png"  alt="" />' +
+					'<img class="magic" src="http://' + imgBucket + '.' + endpoint + '/' + item[i].spines[k].cover + '" />'
+				try {
+					for(var j = 0; j < this.spineInfoArr.length; j++) {
+						var spineDownCon = '<span class="download"></span>' + '</div>';
+						if(item[i].spines[k].id == this.spineInfoArr[j].jsonContent.id) {
+							spineDownCon = '</div>';
+							break;
+						}
+					}
+					if(this.spineInfoArr.length == 0) {
+						var spineDownCon = '<span class="download"></span>' + '</div>';
+					}
+				} catch(e) {
+					console.log(e.message);
+				}
+
+				spineItemsWrapCon += spineDownCon;
+			}
+			spineItemsWrapCon += '</div>';
+			spineItemsWrap.html(spineItemsWrapCon);
+			$('section').append(spineItemsWrap);
+		}
+
+		this.loadedAnimate();
+
+		var classifyArr = $('section .classify');
+		TweenMax.staggerTo(classifyArr, 1, { opacity: 1, ease: Linear.easeInOut }, 0.3);
+
+		var spineWrapArr = $('section .swiper-slide');
+		TweenMax.staggerFrom(spineWrapArr, 1.5, { scale: 0.7, opacity: 0, ease: Elastic.easeOut }, 0.1);
+
+		var magicSpineArr = $('section .classify').find('.magic');
+		TweenMax.staggerFrom(magicSpineArr, 2, { scale: 2, opacity: 0, ease: Strong.easeOut }, 0.1);
+	},
+	showAllSpine: function() {
+		var that = this;
+		$('section').on('click', '.classify .showAll', function() {
+			window.location = 'list.html';
+			var itemIndex = $(this).parents('.classify').index();
+			var itemInfo = {
+				itemId: that.spineItem[itemIndex].id,
+				itemName: that.spineItem[itemIndex].name
+			};
+			localStorage.setItem("itemInfo", JSON.stringify(itemInfo));
+
+		});
+	},
+	downLoadSpine: function() {
+		var that = this;
+		$('section').on('click', '.classify .swiper-slide', function() {
+			var download = $(this).find('.download');
+			download.addClass('loading');
+			TweenMax.to(download, .9, {
+				rotation: 360,
+				ease: Linear.easeOut,
+				repeat: -1
+			});
+			var x = $(this).parents('.classify').index(),
+				y = $(this).index();
+			//js调安卓
+			window.MStore.spineWillDownload($(this).data('source'), JSON.stringify(that.spineItem[x].spines[y]));
+
+			//that.spineDidDownload($(this).data('source'), 'urlurlurl');
+
+		});
+	},
+	spineDidDownload: function(source, url) {
+		//安卓调js 
+		console.log('spineDidDownload');
+		if(source && url) {
+			var downSpine = $('section .swiper-container .swiper-slide[data-source="' + source + '"]'),
+				download = downSpine.find('.download');
+
+			setTimeout(function() {
+				TweenMax.to(download, .3, {
+					scale: 2,
+					opacity: 0,
+					ease: Strong.easeOut,
+					onComplete: function() {
+						download.hide();
+					}
+				});
+			}, 500);
+
+//			var itemArr = [];
+//			var spineInfo = {
+//				id: downSpine.data('id'),
+//				source: source,
+//				url: url
+//			}
+//			this.spineInfoArr.push(spineInfo);
+//			localStorage.setItem("spineInfo", JSON.stringify(this.spineInfoArr));
+		}
+	}
+};
+loadAndFetchSpine.init();
---
+++ js/list.js
@@ -0,0 +1,147 @@
+var loadAndFetchSpine = {
+	init: function() {
+		this.itemInfo = JSON.parse(localStorage.getItem("itemInfo"));
+		this.spineInfoArr = [];
+		this.itemListPage = 1;
+		this.spineItem;
+
+		this.fetchItemList();
+		this.backAndDown();
+	},
+	fetchItemList: function() {
+		var that = this;
+		$.ajax({
+			type: "GET",
+			async: true,
+			data: {
+				page: this.itemListPage,
+				pageSize: 29,
+				sortField: "id",
+				sortOrder: "desc"
+			},
+			url: "https://mapi.magic-store.cn/mstore/spine/category/" + this.itemInfo.itemId,
+			success: function(res) {
+				if(res.code == 200) {
+					console.log(res)
+					try {
+						window.MStore.spineDomains(res.pkgBucket, res.imgBucket, res.endpoint);
+					} catch(e) {
+						console.log(e.message)
+					} 
+					that.spineItem = res.entities[0];
+					that.createItemList(res.entities[0], res.imgBucket, res.endpoint);
+				}
+			}
+		});
+	},
+	spineLoadedList: function(list) {
+		console.log(list);
+		this.spineInfoArr = JSON.parse(list);
+	},
+	createItemList: function(item, imgBucket, endpoint) {
+		console.log(item)
+		var content = '';
+		$('section ul.scrollWrap').find('.itemName i').html(this.itemInfo.itemName);
+		for(var i = 0; i < item.list.length; i++) {
+			var list = $('<li class="item" data-src="http://magic-resource.oss-cn-shanghai.aliyuncs.com/' + item.list[i].cover + '" data-source="' + item.list[i].source + '" data-page="' + item.currentPage + '" data-id="'+ item.list[i].id  +'"></li>');
+			content = '<img class="border" src="img/MS_icon_asset_02.png" alt="" />' +
+							'<span class="download"></span>';
+			for(var k = 0; k < this.spineInfoArr.length; k++) {
+				if(this.spineInfoArr[k].jsonContent.id == item.list[i].id) {
+					content = '<img class="border" src="img/MS_icon_asset_02.png" alt="" />';
+					break; 
+				}
+			}  
+			content += '<img class="magic" data-page="'+item.currentPage+'" src="http://' + imgBucket + '.' + endpoint + '/' + item.list[i].cover+'"/>';
+			list.html(content); 
+			$('ul.scrollWrap').append(list);
+			
+		}
+		if(this.spineItem.hasNextPage && !this.spineItem.isLastPage) {
+			$('section aside').html('正在加载...');
+		} else {
+			$('section aside').html('已显示全部');
+		}
+		this.loadMore();      
+		this.spineAnimate(item.currentPage);
+	},
+	spineAnimate: function(index) {
+		var spineItemArr = $('section .item[data-page="' + index + '"]');
+		TweenMax.staggerFrom(spineItemArr, 1.5, {
+			scale: 0.7,
+			opacity: 0,
+			ease: Elastic.easeOut,
+			onComplete: function() {
+				TweenMax.to($('section aside'), 1, { opacity: 1, ease: Strong.easeOut });
+			}
+		}, 0.1); 
+
+		var magicSpineArr = spineItemArr.find('.magic[data-page="' + index + '"]');
+		TweenMax.staggerFrom(magicSpineArr, 2, { scale: 2, opacity: 0, ease: Strong.easeOut }, 0.1);
+	},
+	loadMore: function() {
+		var that = this, asideTop = $('section').find('aside').position().top;
+		$('section').on('scroll', function() {
+			if(asideTop - $('section').scrollTop() < document.documentElement.clientHeight) {
+				if(that.spineItem.hasNextPage && !that.spineItem.isLastPage) {
+					that.itemListPage++;
+					setTimeout(function() {
+						that.fetchItemList(); 
+					}, 300);
+					$('section').off('scroll');
+				}
+			}
+		});
+	},
+	backAndDown: function() {
+		var that = this;
+		//返回
+		$('.backStore').click(function() {
+			window.location = 'index.html';
+		});
+		//下载
+		$('section .scrollWrap').on('click', '.item', function() {
+			var download = $(this).find('.download');
+			download.addClass('loading');
+			TweenMax.to(download, .9, {
+				rotation: 360,
+				ease: Linear.easeOut,
+				repeat: -1
+			});
+			
+			window.MStore.spineWillDownload($(this).data('source'));
+//			that.spineDidDownload($(this).data('source'), 'urlurlurl');
+		})
+	},
+	spineDidDownload: function(source, url) {
+		console.log('list spineDidDownload');
+		
+		if(source && url) {
+			var downSpine = $('section .scrollWrap .item[data-source="' + source + '"]'),
+				download = downSpine.find('.download');
+			setTimeout(function() {
+				TweenMax.to(download, .3, {
+					scale: 2,
+					opacity: 0,
+					ease: Strong.easeOut,
+					onComplete: function() {
+						download.hide();
+					}
+				});
+			}, 500);
+			
+//			var itemArr = [];   
+//			var spineInfo = {
+//				id: downSpine.data('id'),
+//				source: source,
+//				url: url
+//			}
+//			this.spineInfoArr.push(spineInfo);
+//			localStorage.setItem("spineInfo", JSON.stringify(this.spineInfoArr));
+		}
+		
+		
+	}
+};
+loadAndFetchSpine.init();
+
