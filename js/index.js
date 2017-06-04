d9471f41337ea2ad066dc533b136b834b241c1d5
ZhouYaJie <609276681@qq.com>
2017-06-04 11:44:06 +0800

    adjusting

--- js/index.js
+++ js/index.js
@@ -8,23 +8,12 @@ var loadAndFetchSpine = {
 			$('<div class="loadItem"></div>').prependTo($("section").find('#loading'));
 		}
 
-		this.loadingAndBanner();
+		this.loadingAnimate();
 		this.fetchSpineInfo();
 		this.showAllSpine();
 		this.downLoadSpine();
 	},
-	loadingAndBanner: function() {
-		var banner = $('.swiper-container.banner');
-		TweenMax.to(banner, 1, {
-			opacity: 1,
-			ease: Strong.easeOut,
-			onComplete: function() {
-				var bannerSwiper = new Swiper($('.swiper-container.banner'), {
-					autoplay: 3500,
-					loop: true,
-				});
-			}
-		});
+	loadingAnimate: function() {
 		var load = $('section').find(' #loading .loadItem');
 		TweenMax.staggerTo(load, .2, { scaleY: 1.6, repeat: -1, yoyo: true, ease: Linear.easeInOut }, 0.1);
 	},
@@ -72,7 +61,7 @@ var loadAndFetchSpine = {
 		for(var i = 0; i < item.length; i++) {
 			var spineItemsWrap = $('<div class="classify swiper-container"></div>');
 			spineItemsWrap.attr('data-categoryid', item[i].id);
-			var spineItemsWrapCon = '<p><i>' + item[i].name + '</i><span class="showAll">显示全部</span></p> <div class="swiper-wrapper">'
+			var spineItemsWrapCon = '<p><i>' + item[i].name + '</i><span class="showAll">显示更多</span></p> <div class="swiper-wrapper">'
 
 			var spineLength = 5;
 			if(item[i].spines.length <= spineLength) {
@@ -80,9 +69,8 @@ var loadAndFetchSpine = {
 			}
 
 			for(var k = 0; k < spineLength; k++) {
-				spineItemsWrapCon += '<div class="swiper-slide" data-source="' + item[i].spines[k].source + '" data-id="' + item[i].spines[k].id + '">' +
-					'<img class="border" src="img/MS_icon_asset_02.png"  alt="" />' +
-					'<img class="magic" src="http://' + imgBucket + '.' + endpoint + '/' + item[i].spines[k].cover + '" />'
+				spineItemsWrapCon += '<div class="swiper-slide" data-source="' + item[i].spines[k].source + '" data-id="' + item[i].spines[k].id + '">'
+													//+ '<img class="magic" src="http://' + imgBucket + '.' + endpoint + '/' + item[i].spines[k].cover + '" />'
 				try {
 					for(var j = 0; j < this.spineInfoArr.length; j++) {
 						var spineDownCon = '<span class="download"></span>' + '</div>';
